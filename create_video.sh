68312a88aa84ebb09ae73ddcc431692c3dfbdf8f
Bumblebee00 <mikamatto03@gmail.com>
2025-01-20 01:57:47 +0100

    better way to draw lines

--- create_video.sh
+++ create_video.sh
@@ -1,6 +1,6 @@
-mkdir strano
-cd strano
-gcc ../main.c -o stranoo
-./stranoo
+mkdir migliori_linee
+cd migliori_linee
+gcc ../main.c
+./a.out
 ffmpeg -i %d.png -pix_fmt yuv420p output.mp4
 rm *.png
--- main.c
+++ main.c
@@ -12,9 +12,12 @@
 long double l = 1;
 long double f = 1;
 
+
 void print_vector(Vector v){
     printf("(%Lf, %Lf, %Lf)",v.x,v.y,v.z);
 }
+
+
 
 typedef struct Camera{
     Vector
@@ -56,12 +59,49 @@ void draw_point(unsigned char *img, Vector P, Camera cam){
     }
 }
 
-// brutally inefficient way of drawing a line
+int line_thickness = 5;
 void draw_line(unsigned char *img, Vector A, Vector B, Camera cam){
-    int n_points = mag(vec_sub(A,B))*WIDTH;
+    Vector ac = vec_sub(A, cam.C);
+    Vector Sa = vec_sum(cam.C,vec_scalar(ac, square_mag(cam.D)/dot(cam.D,ac)));
+    long double x_coord_a = dot(cam.R, vec_sub(Sa, cam.C1))*l*WIDTH;
+    long double y_coord_a = dot(cam.U, vec_sub(Sa, cam.C1))*(-l)*HEIGHT;
+    Vector A_2d = creaete_vector(x_coord_a, y_coord_a, 0.);
+    
+    Vector bc = vec_sub(B, cam.C);
+    Vector Sb = vec_sum(cam.C,vec_scalar(bc, square_mag(cam.D)/dot(cam.D,bc)));
+    long double x_coord_b = dot(cam.R, vec_sub(Sb, cam.C1))*l*WIDTH;
+    long double y_coord_b = dot(cam.U, vec_sub(Sb, cam.C1))*(-l)*HEIGHT;
+    Vector B_2d = creaete_vector(x_coord_b, y_coord_b, 0.);
+    
+    Vector BA_2d = vec_sub(A_2d,B_2d);
 
-    for (int i=0; i<n_points; i++){
-        draw_point(img, vec_sum(A, vec_scalar(vec_sub(B,A), (long double)i/n_points)), cam);
+    int index=0;
+    Vector P_2d = creaete_vector(0.,0.,0.);
+    Vector BP_2d;
+    Vector AP_2d;
+    long double dist = 0;
+    for (int i=0; i<HEIGHT; i++){
+        for (int j=0; j<WIDTH; j++){
+            P_2d.x = j+0.5; P_2d.y = i+0.5;
+
+            AP_2d = vec_sub(P_2d,A_2d);
+            BP_2d = vec_sub(P_2d,B_2d);
+
+            if(dot(AP_2d, BA_2d) > 0){
+                dist = mag(AP_2d);
+            } else if (dot(BP_2d, BA_2d) < 0){
+                dist = mag(BP_2d);
+            } else {
+                dist = mag(vec_sub(BP_2d, vec_scalar(BA_2d, dot(BA_2d, BP_2d)/square_mag(BA_2d))));
+            }
+
+            if(roundl(dist)<line_thickness){
+                img[index] = 255;
+                img[index+1] = 255;
+                img[index+2] = 255;
+            }
+            index += 3;
+        }
     }
 }
 
@@ -72,20 +112,21 @@ int main(int argc, char **argv){
     Vector j = creaete_vector(0.,1.,0.);
     Vector k = creaete_vector(0.,0.,1.);
 
-    img = calloc(WIDTH * HEIGHT * 3, sizeof(unsigned char));
 
-    int index = 0;
-    for (int i=0; i<HEIGHT; i++){
-        for (int j=0; j<WIDTH; j++){
-            int c = rand() % 255;
-            img[index] = c;
-            img[index+1] = c;
-            img[index+2] = c;
-            index += 3;
-        }
-    }
+    // // randomize image
+    // int index = 0;
+    // for (int i=0; i<HEIGHT; i++){
+    //     for (int j=0; j<WIDTH; j++){
+    //         int c = rand() % 255;
+    //         img[index] = c;
+    //         img[index+1] = c;
+    //         img[index+2] = c;
+    //         index += 3;
+    //     }
+    // }
 
-    for (int frame=0;frame<200; frame++){
+    for (int frame=0;frame<150; frame++){
+        img = calloc(WIDTH * HEIGHT * 3, sizeof(unsigned char));
 
         Vector C = vec_sum(vec_scalar(k,4.), vec_sum(vec_scalar(i,5.*sin(frame/15.)), vec_scalar(j,5.*cos(frame/15.))));
         Vector D = vec_scalar(C, -f/mag(C)); // camera looking direction
@@ -112,6 +153,8 @@ int main(int argc, char **argv){
     
         if (stbi_write_png(name, WIDTH, HEIGHT, 3, img, WIDTH * 3) != 0) {
         } else { printf("Failed to save the image\n"); }
+
+        free(img);
     }
 
     return 0;
